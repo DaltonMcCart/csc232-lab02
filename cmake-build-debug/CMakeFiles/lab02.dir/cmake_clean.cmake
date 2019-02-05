7fdcd249a9b723df93e1a3b42ff5473a818457e0
Mr. Daehn <jdaehn@missouristate.edu>
2019-02-05 12:37:19 -0600

    LAB02 - Initial import of source files.

---
+++ cmake-build-debug/CMakeFiles/lab02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab02.dir/src/main/cpp/Main.cpp.o"
+  "CMakeFiles/lab02.dir/src/main/cpp/Cell.cpp.o"
+  "../out/lab02.pdb"
+  "../out/lab02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main/cpp/ExpandedTemplates.h
@@ -0,0 +1,15 @@
+/**
+ * CSC232 - Data Structures
+ * Missouri State University, Spring 2019.
+ *
+ * Lab02 - Strategies for Compiling Template Classes
+ *
+ * @file   ExpandedTemplates.h
+ * @brief  Cell implementation.
+ * @author Jim Daehn <jdaehn@missouristate.edu>
+ */
+
+#include "Cell.cpp"
+
+/* Explicit declarations of template classes */
+template class Cell<int>;
